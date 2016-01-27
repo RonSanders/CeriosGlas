ed4ee18c411fcafa55a15ed40889a4e03173afdc
RonSanders <Ron.sanders@cerios.nl>
2016-01-27 23:41:31 +0100

    Het BierGlas object gekopieerd en met hashcode aangeduid dat het een kopie is

--- src/nl/cerios/glaswerk/BierGlas.java
+++ src/nl/cerios/glaswerk/BierGlas.java
@@ -1,8 +1,5 @@
 package nl.cerios.glaswerk;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author rsanders
  *
@@ -28,32 +25,19 @@ public class BierGlas extends Glas implements GlasIF, Cloneable  {
 	}
 
 	/**
-	 * Hij kopieert nu een leeg biertje object, deze moet gekoppeld worden 
-	 * aan de inhoud van het dienblad dacht ik. Echter snap ik niet goed welk
-	 * object bedoeld wordt die gekopieerd moet worden?
-	 * Is dit de goed richting?
-	 * Waarom wil je dit doen? BierGlazen worden gemaakt in de Glasfabriek?
-	 * Is er ipv de clone methode ook een andere manier om objecten te kopieren?
+	 *	Derek Banas T16
 	 * @return 
 	 */
 	@Override
 	public GlasIF copy() {
-				
-		GlasFabriek glasFabriek = new GlasFabriek();
+		BierGlas biertje;
 		
-		List<GlasIF> bierlijst = new ArrayList<GlasIF>();	
-		bierlijst.addAll(glasFabriek.maakDienblad());
-		List<GlasIF> dienblad = glasFabriek.maakDienblad();
-		
-		
-		return (GlasIF) dienblad;
-		//return (GlasIF) bierlijst;	
-		
-		//Krijg constant een ClassCastException, maar weet niet waar ik verkeerd zit? Heb je een tip?
-		//Exception in thread "main" java.lang.ClassCastException: java.util.ArrayList cannot be cast to nl.cerios.glaswerk.GlasIF
-		//at nl.cerios.glaswerk.BierGlas.copy(BierGlas.java:49)
-		//at nl.cerios.glaswerk.TafelService.dek(TafelService.java:32)
-		//at nl.cerios.glaswerk.Persoon.main(Persoon.java:12)
-
+		try{
+			biertje = (BierGlas)super.clone();
+			
+		}catch(CloneNotSupportedException e){	
+			return null;
+		}
+		return biertje;
 	}
 }
--- src/nl/cerios/glaswerk/TafelService.java
+++ src/nl/cerios/glaswerk/TafelService.java
@@ -13,31 +13,38 @@ import java.util.List;
  * @Todo dienblad.size(); object. functienaam en kijken wat een functie kan doen!
  * ?Implementatie: vraagt de GlasFabriek een dienblad te maken en telt de inhoud van de glazen op.
  */
-public class TafelService { // geen implements want het is geen type glas!!!!
+public class TafelService{ // geen implements want het is geen type glas!!!!
 	
 	GlasFabriek glasFabriek = new GlasFabriek();
 	float inhoudTotaalGlazen;
 	
 	public float dek() {
 		List<GlasIF> dienblad = glasFabriek.maakDienblad();
+		BierGlas nieuwBiertje = new BierGlas();
 		
-		//Glas serveren
-		if (dienblad.size()>0) {//hier moet toch ipv length... size(gebruikt worden?)
-		dienblad.remove(dienblad.size()-1);
+		// Glas serveren
+		if (!dienblad.isEmpty()) {
+			dienblad.remove(dienblad.size()-1);
 		}
+		
+		//nieuw extra biertje met een nieuw object, het wordt alleen niet op het dienblad geleverd.
+		BierGlas nieuwBiertje2 = (BierGlas)nieuwBiertje.copy();
+			
+		System.out.println(nieuwBiertje.getContents());
+		System.out.println(nieuwBiertje2.getContents());
+		
+		System.out.println(nieuwBiertje.hashCode());
+		System.out.println(nieuwBiertje2.hashCode());
+		
 		
 		for (GlasIF glas: dienblad) {
 			glas.was();
 			
-			glas.copy();
 			float inhoudGlas = glas.getContents();
-			
 			inhoudTotaalGlazen += inhoudGlas;		
 			
 			System.out.println("Het inhoud per glas: "+inhoudGlas);
 		}
-		
-		
 		return inhoudTotaalGlazen;
 	}
 
